ca62889a264474ca16f7662dfbb1aa5f242580eb
Garfa9909 <rafabarafa@gmail.com>
2026-05-09 13:43:55 +0100

    Revert "ficha 10"
    This reverts commit 21ae0eb0f261c2a17fde73d6690731e0fa39d382.

--- portfolio/models.py
+++ portfolio/models.py
@@ -66,10 +66,10 @@ class Project(models.Model):
         return self.name
 
 class Tfc(models.Model):
-    title = models.CharField(max_length = 1000)
-    author = models.CharField(max_length = 1000)
+    title = models.CharField(max_length = 100)
+    author = models.CharField(max_length = 100)
     year = models.IntegerField()
-    description = models.TextField()
+    description = models.CharField(max_length = 1000)
     degree = models.ForeignKey(Degree, on_delete = models.CASCADE, related_name = "tfcs")
 
     def __str__(self):
